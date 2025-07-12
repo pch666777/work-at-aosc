efb52f1d1ea2a31dc8a49f3a4ffd1fea83f9c097
pch666777 <1005100717@qq.com>
2025-07-12 08:06:14 +0800

    merga link.sh

--- 00no-merga/01work/link.sh
+++ 00no-merga/01work/link.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 relink_dir(){
-    if [ ! -d "$BASE_DIR/$1" ]; then
+    if [ ! -e "$BASE_DIR/$1" ]; then
         echo "目录不存在：$1"
         return 0
     fi
-    if test -d "$BASE_DIR/$1/SRCS"; then
+    if test -e "$BASE_DIR/$1/SRCS"; then
         echo "清理：$1/SRCS"
         rm -rf "$BASE_DIR/$1/SRCS"
     fi
-    if test -d "$BASE_DIR/$1/TREE"; then
+    if test -e "$BASE_DIR/$1/TREE"; then
         echo "清理 $1/TREE"
         rm -rf "$BASE_DIR/$1/TREE"
     fi
@@ -17,14 +17,59 @@ relink_dir(){
     ln -s "$BASE_DIR/base/SRCS" "$BASE_DIR/$1/SRCS"
     echo "$BASE_DIR/base/SRCS --> $BASE_DIR/$1/SRCS"
 
-    ln -s "$BASE_DIR/base/aosc-TREE" "$BASE_DIR/$1/TREE"
-    echo "$BASE_DIR/base/aosc-TREE --> $BASE_DIR/$1/TREE"
+    ln -s "$BASE_DIR/base/$2" "$BASE_DIR/$1/TREE"
+    echo "$BASE_DIR/base/$2 --> $BASE_DIR/$1/TREE"
 }
+
+clear_dir_one(){
+    if [ ! -e "$BASE_DIR/$1" ]; then
+        echo "目录不存在：$1"
+        return 0
+    fi
+    cd "$BASE_DIR/$1"
+    echo "准备清理：$1"
+    sudo ciel clean
+}
+
+clear_dir(){
+    clear_dir_one "rv"
+    clear_dir_one "amd64"
+    clear_dir_one "arm64"
+    clear_dir_one "ppc"
+    clear_dir_one "loong3"
+    clear_dir_one "loong64"
+}
+
+link_asoc(){
+    relink_dir "rv" "aosc-TREE"
+    relink_dir "amd64" "aosc-TREE"
+    relink_dir "arm64" "aosc-TREE"
+    relink_dir "ppc" "aosc-TREE"
+    relink_dir "loong3" "aosc-TREE"
+    relink_dir "loong64" "aosc-TREE"
+}
+
+link_my_asoc(){
+    relink_dir "rv" "my-TREE"
+    relink_dir "amd64" "my-TREE"
+    relink_dir "arm64" "my-TREE"
+    relink_dir "ppc" "my-TREE"
+    relink_dir "loong3" "my-TREE"
+    relink_dir "loong64" "my-TREE"
+}
+
 
 BASE_DIR=$(pwd)
 echo "当前工作目录：$BASE_DIR"
-relink_dir "rv"
-relink_dir "amd64"
-relink_dir "ppc"
-relink_dir "loong3"
-relink_dir "loong64"
+
+echo "1-连接 aosc 目录"
+echo "2-连接 my-aosc 目录"
+echo "3-清理所有构架"
+
+read -p "请输入操作数字 (1-3): " choice
+case $choice in
+    1) link_asoc ;;
+    2) link_my_asoc ;;
+    3) clear_dir ;;
+    *) echo "无效输入" ;;
+esac
